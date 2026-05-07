ccbf5efaebcb46b764a018fb8838c593b73eb1cd
Kothapalli Durga Bhavani <durgabhavanik2005@gmail.com>
2026-05-07 12:05:07 +0530

    Removed old week files from Task 1 branch

--- Plotly.py
+++
@@ -1,8 +0,0 @@
-import plotly.express as px
-import seaborn as sns
-
-df = sns.load_dataset("iris")
-
-fig = px.scatter(df, x="sepal_length", y="petal_length", color="species")
-
-fig.show(renderer="browser")
